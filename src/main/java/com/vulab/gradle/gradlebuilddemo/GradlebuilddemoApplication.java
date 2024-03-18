52e42662bb16237ab29a7dd87df71d9bd466c878
saravanan <saravanan_kuppusamy@yahoo.com>
2024-03-18 07:34:33 -0400

    vulab gradle build demo

---
+++ src/main/java/com/vulab/gradle/gradlebuilddemo/GradlebuilddemoApplication.java
@@ -0,0 +1,13 @@
+package com.vulab.gradle.gradlebuilddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlebuilddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlebuilddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vulab/gradle/gradlebuilddemo/GradlebuilddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulab.gradle.gradlebuilddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlebuilddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
